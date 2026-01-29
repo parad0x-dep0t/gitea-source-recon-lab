9d1f583ec71d4756a44a8ab7c33fc7e78702b9f9
parad0x-dep0t <158147172+parad0x-dep0t@users.noreply.github.com>
2026-01-29 19:29:07 +0530

    Update scanner.py

--- src/scanner.py
+++ src/scanner.py
@@ -1,1 +1,18 @@
+"""
+scanner.py
+
+MVP source code scanner.
+This file will handle:
+- Directory traversal
+- File reading
+- Applying detection rules
+- Printing findings
+"""
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
 
